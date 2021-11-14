75521dac73b783850c7f468b17a2e4673ae616f7
p-sam <p-sam@d3vs.net>
2021-11-14 12:45:32 +0000

    Add project consistency check script

---
+++ check.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -eo pipefail
+
+source "$( dirname "${BASH_SOURCE[0]}" )/_env.sh"
+
+LUA_BIN="$(which luajit || which lua ||:)"
+
+if [[ -z "$LUA_BIN" ]]; then
+	echo "No lua interpreter found in PATH" 1>&2
+	exit 1
+fi
+
+echo "Using lua interpreter $LUA_BIN"
+
+cd "$ROOT_DIR/repo/love/src/scripts"
+
+for f in *.lua; do
+	if [[ -f "$f.h" ]]; then
+		"$LUA_BIN" auto.lua "$f"
+	fi
+done
+
+CHANGED_LUA_HEADERS="$(git status --porcelain --untracked-files=no | grep '.lua.h')"
+
+if [[ -n "$CHANGED_LUA_HEADERS" ]]; then
+	echo -e "Unsynced lua headers found:\n$CHANGED_LUA_HEADERS" 1>&2
+	exit 2
+fi
